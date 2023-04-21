9f02ce65cd4ec51a580abb6a883f0e189781ab3a
Jack Darker <JackDarker@freenet.de>
2023-04-21 18:27:43 +0200

    load tilemap

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -39,9 +39,7 @@ class MainMenu extends Phaser.Scene {
         this.loadText.setShadow(2, 2, '#2d2d2d', 4, true, false);
     }
     create(){
-        this.input.keyboard.on('keyup-ESC', () => {
-            this.scene.switch('GameScene');
-        }, this);
+        this.input.keyboard.on('keyup-ESC', () => {this.scene.switch('GameScene');}, this);
         /*this.input.keyboard.once('keydown-SPACE', () => {
             this.scene.start('GameScene');
         }, this);
@@ -50,6 +48,28 @@ class MainMenu extends Phaser.Scene {
 
         });*/
     }
+}
+class Player extends Phaser.Physics.Arcade.Sprite{//Phaser.GameObjects.Sprite {
+    constructor (game,x,y) {
+        super(game,x, y, 'dude');
+        game.physics.add.existing(this);
+        game.physics.world.enable(this);
+        //this=game.physics.add.sprite(x, y, 'dude');
+        this.onLadder = false;
+        //this.player.setBounce(0.2);
+        this.body.setGravityY(300)
+        this.addToDisplayList() //TODO why is this not automatically added
+    }
+    update(){
+        super.update();
+        if (this.onLadder) {
+            this.body.gravity.y = 0;
+            } else {
+            this.body.gravity.y = 300;
+            }
+            this.onLadder = false;
+    }
+
 }
 
 class GameScene extends Phaser.Scene
@@ -65,9 +85,143 @@ class GameScene extends Phaser.Scene
         this.load.image('star', 'assets/sprites/star.png');
         this.load.image('block', 'assets/sprites/crate.png');
         this.load.spritesheet('dude', 'assets/sprites/dude.png', { frameWidth: 32, frameHeight: 48 });
-    }
+        this.load.spritesheet('hero', 'assets/Warrior/SpriteSheet/Warrior_SheetnoEffect.png', { frameWidth: 69, frameHeight: 44 });
 
+        this.load.image('kenny_platformer_64x64', 'assets/atlas/kenny_platformer_64x64.png');
+        this.load.tilemapTiledJSON('multiple-layers-map', 'assets/maps/multiple-layers.json');
+    }
+    setupAnims(){
+        var a=0,b=0;
+        {//dude
+            this.anims.create({
+                key: 'left',
+                frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+                frameRate: 10,
+                repeat: -1
+            });
+            this.anims.create({
+                key: 'turn',
+                frames: [ { key: 'dude', frame: 4 } ],
+                frameRate: 20
+            });
+            this.anims.create({
+                key: 'right',
+                frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+                frameRate: 10,
+                repeat: -1
+            });
+        }
+        /*{//hero
+            a=0,b=6;
+            this.anims.create({
+                key: 'idle',
+                frames: this.anims.generateFrameNumbers('hero', { start: a, end: b }),
+                frameRate: 10,
+                repeat: -1
+            });
+            a=a+b,b=a+8;
+            this.anims.create({
+                key: 'right',
+                frames: this.anims.generateFrameNumbers('hero', { start: a, end: b }),
+                frameRate: 10,
+                repeat: -1
+            });
+            a=a+b,b=a+12;
+            this.anims.create({
+                key: 'attack1',
+                frames: this.anims.generateFrameNumbers('hero', { start: a, end: b }),
+                frameRate: 10,
+                repeat: -1
+            });
+            a=a+b,b=a+11;
+            this.anims.create({
+                key: 'death',
+                frames: this.anims.generateFrameNumbers('hero', { start: a, end: b }),
+                frameRate: 10,
+                repeat: -1
+            });
+            a=a+b,b=a+4;
+            this.anims.create({
+                key: 'hurt',
+                frames: this.anims.generateFrameNumbers('hero', { start: a, end: b }),
+                frameRate: 10,
+                repeat: -1
+            });
+            a=a+b,b=a+3;
+            this.anims.create({
+                key: 'jump',
+                frames: this.anims.generateFrameNumbers('hero', { start: a, end: b }),
+                frameRate: 10,
+                repeat: -1
+            });
+            a=a+b,b=a+2;//uptofall
+            a=a+b,b=a+3;
+            this.anims.create({
+                key: 'fall',
+                frames: this.anims.generateFrameNumbers('hero', { start: a, end: b }),
+                frameRate: 10,
+                repeat: -1
+            });
+            a=a+b,b=a+5;
+            this.anims.create({
+                key: 'edgegrab',
+                frames: this.anims.generateFrameNumbers('hero', { start: a, end: b }),
+                frameRate: 10,
+                repeat: -1
+            });
+            a=a+b,b=a+6;
+            this.anims.create({
+                key: 'edgegrabidle',
+                frames: this.anims.generateFrameNumbers('hero', { start: a, end: b }),
+                frameRate: 10,
+                repeat: -1
+            });
+            a=a+b,b=a+3;
+            this.anims.create({
+                key: 'wallslide',
+                frames: this.anims.generateFrameNumbers('hero', { start: a, end: b }),
+                frameRate: 10,
+                repeat: -1
+            });
+            a=a+b,b=a+6;
+            this.anims.create({
+                key: 'crouch',
+                frames: this.anims.generateFrameNumbers('hero', { start: a, end: b }),
+                frameRate: 10,
+                repeat: -1
+            });
+            a=a+b,b=a+7;
+            this.anims.create({
+                key: 'dash',
+                frames: this.anims.generateFrameNumbers('hero', { start: a, end: b }),
+                frameRate: 10,
+                repeat: -1
+            });
+            a=a+b,b=a+10;
+            this.anims.create({
+                key: 'attack3',
+                frames: this.anims.generateFrameNumbers('hero', { start: a, end: b }),
+                frameRate: 10,
+                repeat: -1
+            });
+            a=a+b,b=a+5;
+            this.anims.create({
+                key: 'slide',
+                frames: this.anims.generateFrameNumbers('hero', { start: a, end: b }),
+                frameRate: 10,
+                repeat: -1
+            });
+            a=a+b,b=a+8;
+            this.anims.create({
+                key: 'ladder',
+                frames: this.anims.generateFrameNumbers('hero', { start: a, end: b }),
+                frameRate: 10,
+                repeat: -1
+            });
+        }*/
+    }
     create () {
+        this.cursors = this.input.keyboard.createCursorKeys();
         //  Set the camera and physics bounds to be the size of 4x4 bg images
         this.cameras.main.setBounds(-Const.gameWidth/2, -Const.gameHeight/2, Const.gameWidth * 1, Const.gameHeight * 1);
         this.physics.world.setBounds(-Const.gameWidth/2, -Const.gameHeight/2, Const.gameWidth * 1, Const.gameHeight * 1);
@@ -77,39 +231,55 @@ class GameScene extends Phaser.Scene
         //g_1.setTileScale(1,1);  //can only be <1?
         this.scene.launch('UIScene'); //run UI as parallel scene
 
-        for (let i = 0; i < 64; i++)  {
+        /*for (let i = 0; i < 64; i++)  {
             const x = Phaser.Math.Between(-Const.gameWidth/2+20, Const.gameWidth/2-20);
             const y = Phaser.Math.Between(-Const.gameHeight/2+20, Const.gameHeight/2-20);
             const box = this.add.image(x, y, 'star');
             //  Make them all input enabled
             box.setInteractive();
-        }
+        }*/
+        this.map = this.make.tilemap({ key: 'multiple-layers-map' });
+        const tiles = this.map.addTilesetImage('kenny_platformer_64x64');
+
+        this.rockLayer = this.map.createLayer('Rock Layer', tiles, 0, 0);
+        this.waterLayer = this.map.createLayer('Water Layer', tiles, 0, 0);
+        this.platformLayer = this.map.createLayer('Platform Layer', tiles, 0, 0);
+        this.stuffLayer = this.map.createLayer('Stuff Layer', tiles, 0, 0);
+        //this.rockLayer.setCollisionByProperty({ collides: true });
+
         let platforms = this.physics.add.staticGroup();
         platforms.create(40, 140, 'ground').setScale(2).refreshBody();
-
-        this.cursors = this.input.keyboard.createCursorKeys();
+        let stars = this.physics.add.group({
+            key: 'star',
+            repeat: 11,
+            setXY: { x: -Const.gameWidth/2+20, y: -Const.gameHeight/2-20, stepX: 70 }
+        });
+        stars.children.iterate(function (child) {
+            child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+        });
+        platforms.add(this.rockLayer);
+        //this.physics.add.collider(stars, this.rockLayer);
+        this.physics.add.collider(stars, platforms);
+        //this.setupAnims();
         this.anims.create({
             key: 'left',
             frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
             frameRate: 10,
             repeat: -1
         });
-        
         this.anims.create({
             key: 'turn',
             frames: [ { key: 'dude', frame: 4 } ],
             frameRate: 20
         });
-        
         this.anims.create({
             key: 'right',
             frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
             frameRate: 10,
             repeat: -1
         });
-        this.player = this.physics.add.sprite(0, 0, 'dude');
-        //this.player.setBounce(0.2);
-        this.player.body.setGravityY(300)
+        this.player = new Player(this,0,0); //this.physics.add.sprite(0, 0, 'dude');// 
+        
         this.player.setCollideWorldBounds(true); //disallow to leave world bounds
         this.physics.add.collider(this.player, platforms);
         this.cameras.main.startFollow(this.player, true);
@@ -129,6 +299,8 @@ class GameScene extends Phaser.Scene
             this.scene.switch('MainMenu');
         }, this);*/
         this.input.on('gameobjectup', this.clickHandler, this);
+        
+        this.physics.add.overlap(this.player, stars, this.collectStar, null, this);
     }
 
     update(){
@@ -157,7 +329,10 @@ class GameScene extends Phaser.Scene
             this.player.setVelocityY(300);
         }*/
     }
-
+    collectStar (player, star) {
+        this.events.emit('addScore');
+        star.disableBody(true, true);
+    }
     clickHandler (pointer, box)
     {
         //  Disable our box
